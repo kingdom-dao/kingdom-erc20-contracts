0b77b20461db8c8546f9b9a1e8074a5cb6be4172
pokotaro.eth <kentarou_masuda@hacklab-tech.io>
2023-10-22 12:26:30 +0900

    ♻️ refactor

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,25 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox-viem'
+import dotenv from 'dotenv'
+dotenv.config()
+
+// API
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY as string
+// NETWORK
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL as string
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY as string
 
 const config: HardhatUserConfig = {
   networks: {
-    polygon: {
-      url: "",
-      accounts: [""],
-    }
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
   },
-  solidity: "0.8.20",
-};
+  etherscan: {
+    apiKey: ETHERSCAN_KEY,
+  },
+  solidity: '0.8.20',
+}
 
-export default config;
+export default config
